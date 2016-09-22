5de7a772f1f38051294250e2c662ba07f9189524
Luis Aznar Ferris <luazfe@floridauniversitaria.es>
2016-09-22 09:57:49 +0200

    primer trabajo

---
+++ indexmod.php
@@ -0,0 +1,16 @@
+    <?php
+//fichero index.php
+//creado por Paco Gomez
+    ?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+  </head>
+  <body>
+   <?php
+        echo "Luis <br>";
+        echo "Aznar <br>";
+        print "Ferris <br>";
+      ?>
+  </body>
+</html>
